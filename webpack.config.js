f64e5254af60e992105b70d6cc158be487a18fa0
Ivar Óli <ivartheoli@gmail.com>
2018-10-24 13:24:35 +0000

    webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var DIST_DIR = path.resolve(__dirname, "dist");
+var SRC_DIR = path.resolve(__dirname, "src/client");
+
+var config = {
+    mode: 'development',
+    entry : SRC_DIR + "/main.js",
+    output: {
+        path: DIST_DIR,
+        filename: "main.min.js"
+    }
+};
+
+module.exports = config;
