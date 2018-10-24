a4c25dcb2062df7bd6659b3231eaaccd72351d23
Ivar Óli <ivartheoli@gmail.com>
2018-10-24 14:18:56 +0000

    game changes player after each turn

--- src/logic/game.js
+++ src/logic/game.js
@@ -2,14 +2,25 @@
  * Determins who's turn it is
  * Holds the game status
  */
+
+//game status
 var gameStatus = [
     ['','',''],
     ['','',''],
     ['','','']
 ];
- //either X or O
+
+//either X or O
 var currentPlayer = 'X';
+
+//changes a single cell in the grid
 module.exports.changeCell = function(x, y){
-    gameStatus[x][y] = currentPlayer;
+    gameStatus[y][x] = currentPlayer;
+    currentPlayer = this.changePlayer(currentPlayer);
     return gameStatus;
+}
+
+//changes the current player
+module.exports.changePlayer = function(player){    
+    return (player == 'X')?'O':'X';
 }
--- src/logic/game.test.js
+++ src/logic/game.test.js
@@ -1,8 +1,19 @@
 const game = require("./game");
- it("should return the game status", () => {
+
+it("should return the game status", () => {
     expect(game.changeCell(0,0)).toMatchObject([
         ['X','',''],
         ['','',''],
         ['','','']
     ]);
+    expect(game.changeCell(1,0)).toMatchObject([
+        ['X','O',''],
+        ['','',''],
+        ['','','']
+    ]);
+});
+
+it("should return the game status", () => {
+    expect(game.changePlayer('X')).toBe('O');
+    expect(game.changePlayer('O')).toBe('X');
 });
