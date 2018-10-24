3b7bc4c814faef5dfee3d349969f7f20ed60991b
Ivar Óli <ivartheoli@gmail.com>
2018-10-24 13:25:48 +0000

    webpack devserver

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,8 @@ var DIST_DIR = path.resolve(__dirname, "dist");
 var SRC_DIR = path.resolve(__dirname, "src/client");
 
 var config = {
+    mode: 'development',
+    entry : SRC_DIR + "/main.js",
     output: {
         path: DIST_DIR,
         filename: "main.min.js"
@@ -13,7 +15,14 @@ var config = {
         new HtmlWebpackPlugin({
             template: SRC_DIR + "/index.html"
         })
-    ]
+    ],
+    devServer: {
+        port: 5000,
+        open: true,
+        proxy: {
+            "/api": "http://localhost:3000"
+        }
+    }
 };
 
 module.exports = config;
