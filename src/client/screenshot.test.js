9c9b6acc6e96bd02a5d972d1589e6ba6df9a8c56
Ivar Óli <ivartheoli@gmail.com>
2018-10-28 21:36:57 +0000

    complete UI testing

--- src/client/screenshot.test.js
+++ src/client/screenshot.test.js
@@ -2,7 +2,7 @@ const puppeteer = require("puppeteer");
 
   describe("Localhost screenshots", () => {
   let browser, page;
-  let url = "http://localhost:3000/";
+  let url = "https://tictactoe-hugb.herokuapp.com/";
 
   beforeEach(async () => {
     browser = await puppeteer.launch({ headless: false, slowMo: 0 });
@@ -13,7 +13,7 @@ const puppeteer = require("puppeteer");
     browser.close();
   });
 
-  test("Test clicks on elements", async () => {
+  test("Test X as winner", async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.setViewport({ width: 1440, height: 800 });
@@ -45,6 +45,18 @@ const puppeteer = require("puppeteer");
     var winner  = await page.evaluate(() => document.querySelector('#winner').innerText);
     expect(winner).toBe('The Winner Is: X');
 
+    //screenshot
+    //await page.screenshot({path: "docs/screenshots/screenshot.png"});
+
+    browser.close();
+  }, 5000);
+
+  test("Tests O as winner", async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1440, height: 800 });
+    await page.goto(url);
+
     //clicks
     await page.click("#c1-1");//x
     var c11 = await page.evaluate(() => document.querySelector('#c1-1').innerText);
@@ -74,18 +86,53 @@ const puppeteer = require("puppeteer");
     //check for winner
     var winner  = await page.evaluate(() => document.querySelector('#winner').innerText);
     expect(winner).toBe('The Winner Is: O');
+  }, 5000);
 
-    //if X has clicked on column 1-1 than O can't click on the same column
-    await page.click("#c1-1"); //x
-    var c11 =  await page.evaluate(() => document.querySelector('#c1-1').innerText);
-    expect(c11).toBe('X');
-    await page.click("#c1-1"); //o
+  test("Tests draw", async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1440, height: 800 });
+    await page.goto(url);
+
+    //clicks
+    await page.click("#c1-1");//x
     var c11 = await page.evaluate(() => document.querySelector('#c1-1').innerText);
-    expect(c11).toBe('X')
+    expect(c11).toBe('X');
 
-    //screenshot
-    await page.screenshot({path: "docs/screenshots/screenshot.png"});
+    await page.click("#c1-2");//0
+    var c12 = await page.evaluate(() => document.querySelector('#c1-2').innerText);
+    expect(c12).toBe('O');
 
-    browser.close();
-  }, 7000);
+    await page.click("#c1-3");//x
+    var c13 = await page.evaluate(() => document.querySelector('#c1-3').innerText);
+    expect(c13).toBe('X');
+
+    await page.click("#c2-1");//o
+    var c21 = await page.evaluate(() => document.querySelector('#c2-1').innerText);
+    expect(c21).toBe('O');
+
+    await page.click("#c2-3");//x
+    var c23 = await page.evaluate(() => document.querySelector('#c2-3').innerText);
+    expect(c23).toBe('X');
+
+    await page.click("#c2-2");//o
+    var c22 = await page.evaluate(() => document.querySelector('#c2-2').innerText);
+    expect(c22).toBe('O');
+
+    await page.click("#c3-1");//x
+    var c31 = await page.evaluate(() => document.querySelector('#c3-1').innerText);
+    expect(c31).toBe('X');
+
+    await page.click("#c3-3");//o
+    var c33 = await page.evaluate(() => document.querySelector('#c3-3').innerText);
+    expect(c33).toBe('O');
+
+    await page.click("#c3-2");//x
+    var c32 = await page.evaluate(() => document.querySelector('#c3-2').innerText);
+    expect(c32).toBe('X');
+    
+    //check for winner
+    var winner  = await page.evaluate(() => document.querySelector('#winner').innerText);
+    expect(winner).toBe('The Winner Is: No one, it\'s a draw');
+  }, 5000);
 });
